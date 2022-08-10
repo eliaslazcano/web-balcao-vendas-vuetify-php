6f4fec0b38481719c230fe9f499cd16412127db3
Elias Lazcano Castro Neto <elias304162@gmail.com>
2022-08-10 15:04:36 -0400

    Editando uma tabela
    A tabela de produtos contidos em uma venda pode ser editada em tempo real (in-line).

--- src/views/VendaFormulario.vue
+++ src/views/VendaFormulario.vue
@@ -8,7 +8,7 @@
             <v-text-field label="Cliente" v-model="iptCliente" :disabled="enviandoVenda" placeholder="Nome do cliente" persistent-placeholder></v-text-field>
           </v-col>
           <v-col cols="12" md="6">
-            <v-text-field label="Valor Pago" v-model="iptCredito" :disabled="enviandoVenda"></v-text-field>
+            <v-text-field label="Valor Pago" v-model="iptCredito" :disabled="enviandoVenda" type="tel"></v-text-field>
           </v-col>
         </v-row>
         <v-autocomplete
@@ -19,20 +19,55 @@
           item-text="nome"
           no-data-text="Nenhum produto corresponde"
           :disabled="enviandoVenda"
-        ></v-autocomplete>
+          auto-select-first
+        >
+          <template v-slot:item="{item}">
+            <v-list-item-content>
+              <v-list-item-title>{{item.nome}}</v-list-item-title>
+              <v-list-item-subtitle>R$ {{item.valor.toFixed(2)}}</v-list-item-subtitle>
+            </v-list-item-content>
+          </template>
+        </v-autocomplete>
       </v-card-text>
       <v-data-table
         :headers="tableHeaders"
         :items="tableItems"
         :search="tableSearch"
         no-data-text="Nenhum produto adicionado"
+        sort-by="produto_nome"
       >
         <template v-slot:top>
           <div class="mx-4">
             <p class="subtitle">Lista de produtos</p>
           </div>
         </template>
-        <template v-slot:[`item.valor`]="{item}">R$ {{ item.valor ? item.valor.toFixed(2) : '0.00' }}</template>
+        <template v-slot:[`item.quantidade`]="{item}">
+          <v-edit-dialog :return-value.sync="item.quantidade">
+            {{ item.quantidade }}
+            <template v-slot:input>
+              <v-text-field
+                v-model="item.quantidade"
+                single-line
+                type="tel"
+                :rules="[v => !!v || 'Insira a quantidade']"
+              ></v-text-field>
+            </template>
+          </v-edit-dialog>
+        </template>
+        <template v-slot:[`item.valor`]="{item}">
+          <v-edit-dialog :return-value.sync="item.valor">
+            R$ {{ item.valor ? parseFloat(item.valor).toFixed(2) : '0.00' }}
+            <template v-slot:input>
+              <v-text-field
+                prefix="R$"
+                v-model="item.valor"
+                single-line
+                type="tel"
+                :rules="[v => !!v || 'Insira o valor']"
+              ></v-text-field>
+            </template>
+          </v-edit-dialog>
+        </template>
         <template v-slot:foot>
           <tfoot>
           <tr>
@@ -48,7 +83,7 @@
         </template>
       </v-data-table>
       <v-card-actions class="justify-center">
-        <v-btn small depressed color="success" :loading="enviandoVenda" @click="salvarVenda">
+        <v-btn small depressed color="success" :disabled="tableItems.length === 0" :loading="enviandoVenda" @click="salvarVenda">
           <v-icon dense class="mr-1">mdi-content-save</v-icon> Salvar
         </v-btn>
       </v-card-actions>
@@ -82,7 +117,7 @@ export default {
   }),
   computed: {
     valorTotal() {
-      return this.tableItems.reduce((previousValue, currentValue) => previousValue + currentValue.valor, 0);
+      return this.tableItems.reduce((previousValue, currentValue) => previousValue + parseFloat(currentValue.valor), 0);
     },
   },
   methods: {
@@ -108,9 +143,8 @@ export default {
       try {
         this.enviandoVenda = true;
         const webclient = http();
-        const {data} = await webclient.post('vendas', {id: this.id, cliente: this.iptCliente, credito: this.iptCredito, itens: this.tableItems});
-        if (!this.id || this.id.toString() !== data.id.toString()) await this.$router.push('/venda/' + data.id);
-        else await this.loadData(false);
+        await webclient.post('vendas', {id: this.id, cliente: this.iptCliente, credito: this.iptCredito, itens: this.tableItems});
+        await this.$router.push('/vendas');
       } finally {
         this.enviandoVenda = false;
       }
@@ -129,8 +163,8 @@ export default {
             'id': null, 'produto': produto.id, 'produto_nome': produto.nome, 'quantidade': 1, 'valor': produto.valor
           });
         } else {
-          this.tableItems[pedidoItemIndex].quantidade += 1;
-          this.tableItems[pedidoItemIndex].valor += produto.valor;
+          this.tableItems[pedidoItemIndex].quantidade = parseFloat(this.tableItems[pedidoItemIndex].quantidade) + 1;
+          this.tableItems[pedidoItemIndex].valor = parseFloat(this.tableItems[pedidoItemIndex].valor) + produto.valor;
         }
         this.$nextTick(() => this.iptProduto = null);
       }
