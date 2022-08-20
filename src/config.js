f7a1730907cba7791223223e2035fe05922227ca
Elias Lazcano Castro Neto <elias304162@gmail.com>
2022-08-20 02:33:46 -0400

    Update config.js

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ export const config = {
     requestTimeout: 60000,
     biometriaBaseUrl: 'http://localhost:9000/api/public/v1/captura/' //URL para se comunicar com o backend do leitor da Nitgen;
   },
-  nome_empresa: null, //string. Nome exibido na barra de ferramentas;
-  biometria: true, //boolean. Habilita a interface para interagir com os leitores da Nitgen;
-  rfid: true, //boolean. Habilita a interface para interagir com leitores de RFID (aproximacao);
+  nome_empresa: 'Viana Climatização', //string. Nome exibido na barra de ferramentas;
+  biometria: false, //boolean. Habilita a interface para interagir com os leitores da Nitgen;
+  rfid: false, //boolean. Habilita a interface para interagir com leitores de RFID (aproximacao);
 };
