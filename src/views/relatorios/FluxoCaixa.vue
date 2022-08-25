82ec9961c71e13e0ca5a2c5592b0afb72b3db635
Elias Lazcano Castro Neto <elias304162@gmail.com>
2022-08-25 01:48:29 -0400

    Relatório de saldo previsto do caixa
    Utilizando as despesas cadastradas e o valor recebido das vendas, podemos ver o saldo teórico do caixa, quantia total de arrecadação e a quantia total de despesas. Um gráfico visual para ficar fácil de enxergar a diferença entre ambos visualmente. O usuário pode escolher um período de duas datas para puxar os dados.

--- src/views/relatorios/FluxoCaixa.vue
+++ src/views/relatorios/FluxoCaixa.vue
@@ -1,29 +1,43 @@
 <template>
   <async-container :loading="loading">
-    <v-card width="32rem" class="mx-auto">
-      <v-card-title>Fluxo de Caixa</v-card-title>
-      <v-card-text>
-        <v-row>
-          <v-col cols="12" sm="6">
-            <date-picker-br inline no-buttons outlined dense hide-details v-model="iptDataInicial" label="Data inicial" prepend-inner-icon="mdi-calendar-arrow-right"></date-picker-br>
-          </v-col>
-          <v-col cols="12" sm="6">
-            <date-picker-br inline no-buttons outlined dense hide-details v-model="iptDataFinal" label="Data limite" prepend-inner-icon="mdi-calendar-arrow-left"></date-picker-br>
-          </v-col>
-        </v-row>
+    <div style="max-width: 32rem" class="mx-auto">
+      <p class="title">Fluxo de Caixa</p>
+      <v-card width="32rem" class="mx-auto" :loading="loadingData">
+        <v-card-title>Período</v-card-title>
+        <v-card-text>
+          <v-row>
+            <v-col cols="12" sm="6">
+              <date-picker-br
+                inline no-buttons outlined dense hide-details v-model="iptDataInicial" label="Data inicial"
+                prepend-inner-icon="mdi-calendar-arrow-right" :disabled="loadingData"
+              ></date-picker-br>
+            </v-col>
+            <v-col cols="12" sm="6">
+              <date-picker-br
+                inline no-buttons outlined dense hide-details v-model="iptDataFinal" label="Data limite"
+                prepend-inner-icon="mdi-calendar-arrow-left" :disabled="loadingData"
+              ></date-picker-br>
+            </v-col>
+          </v-row>
+        </v-card-text>
+        <v-card-actions class="justify-center pt-0" @click="loadData">
+          <v-btn outlined small color="primary">
+            <v-icon small class="mr-1">mdi-refresh</v-icon>
+            Recalcular
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+      <v-card :color="saldo > 0 ? 'success' : 'error'" dark class="my-5">
+        <v-card-text class="d-flex py-3">
+          <v-icon size="3rem" class="mr-2">{{ saldo > 0 ? 'mdi-plus-thick' : 'mdi-minus-thick' }}</v-icon>
+          <div>
+            <p class="subtitle-2 mb-2">Saldo teórico</p>
+            <p class="mb-0" style="font-size: 2rem">R$ {{ formatoMonetario(saldo) }}</p>
+          </div>
+        </v-card-text>
+      </v-card>
+      <v-card class="my-3">
         <v-list outlined dense rounded class="my-5">
-          <v-list-item>
-            <v-list-item-avatar>
-              <v-avatar color="error">
-                <v-icon color="white">mdi-arrow-down-bold</v-icon>
-              </v-avatar>
-            </v-list-item-avatar>
-            <v-list-item-content>
-              <v-list-item-title> {{ despesas.despesas }} Despesas</v-list-item-title>
-              <v-list-item-subtitle>R$ {{ formatoMonetario(despesas.debito) }}</v-list-item-subtitle>
-            </v-list-item-content>
-          </v-list-item>
-          <v-divider></v-divider>
           <v-list-item>
             <v-list-item-avatar>
               <v-avatar color="success">
@@ -35,18 +49,27 @@
               <v-list-item-subtitle>R$ {{ formatoMonetario(vendas.credito) }}</v-list-item-subtitle>
             </v-list-item-content>
           </v-list-item>
+          <v-divider></v-divider>
+          <v-list-item>
+            <v-list-item-avatar>
+              <v-avatar color="error">
+                <v-icon color="white">mdi-arrow-down-bold</v-icon>
+              </v-avatar>
+            </v-list-item-avatar>
+            <v-list-item-content>
+              <v-list-item-title> {{ despesas.despesas }} Despesas</v-list-item-title>
+              <v-list-item-subtitle>R$ {{ formatoMonetario(despesas.debito) }}</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
         </v-list>
-        <v-card outlined :color="saldo > 0 ? 'success' : 'error'" dark>
-          <v-card-text class="d-flex">
-            <v-icon size="3rem" class="mr-2">{{ saldo > 0 ? 'mdi-arrow-up-bold' : 'mdi-arrow-down-bold' }}</v-icon>
-            <div>
-              <p class="subtitle-2 mb-2">Saldo</p>
-              <p class="mb-0" style="font-size: 2rem">R$ {{ formatoMonetario(saldo) }}</p>
-            </div>
-          </v-card-text>
-        </v-card>
-      </v-card-text>
-    </v-card>
+      </v-card>
+      <v-card class="my-3" v-if="!!credito || !!debito">
+        <v-card-text>
+          <p class="title">Comparativo</p>
+          <apex-chart type="donut" :options="chartOptions" :series="series"></apex-chart>
+        </v-card-text>
+      </v-card>
+    </div>
   </async-container>
 </template>
 
@@ -61,12 +84,27 @@ export default {
   components: {DatePickerBr, AsyncContainer},
   data: () => ({
     loading: true,
+    loadingData: true,
     iptDataInicial: moment().format('YYYY-01-01'),
     iptDataFinal: moment().format('YYYY-MM-DD'),
     credito: 0,
     debito: 0,
     vendas: 0,
     despesas: 0,
+    options: {},
+    series: [],
+    chartOptions: {
+      labels: ['Arrecadação','Despesa'],
+      fill: {
+        type: 'gradient',
+      },
+      colors: ['#4caf50', '#ff5252'],
+      tooltip: {
+        y: {
+          formatter: (val) => 'R$ ' + Number(val).toLocaleString('pt-BR', {maximumFractionDigits: 2, minimumFractionDigits: 2})
+        }
+      }
+    },
   }),
   computed: {
     saldo() {
@@ -75,10 +113,12 @@ export default {
   },
   methods: {
     async loadData() {
+      this.loadingData = true;
       try {
         const webclient = http();
         const params = {'data_inicio': this.iptDataInicial, 'data_fim': this.iptDataFinal}
         const {data} = await webclient.get('relatorios/fluxo_caixa', {params});
+        this.series = [data.credito, data.debito];
         this.credito = data.credito;
         this.debito = data.debito;
         this.vendas = data.vendas;
@@ -87,6 +127,7 @@ export default {
       } catch (e) {
         return false;
       } finally {
+        this.loadingData = false;
         this.loading = false;
       }
       return true;
